0d191a939b528a9306f04dead2b6bcf2123893ac
jonathands <jonathands@gmail.com>
2020-05-24 18:29:08 -0300

    reddit react demo app

---
+++ src/Reddit.js
@@ -0,0 +1,87 @@
+
+import axios from "axios";
+import React, { Component } from 'react';
+
+class Reddit extends Component {
+    state = {
+        subs: ['java', 'javascript', 'php', 'csharp', 'react'],
+        subPosts: [],
+        sub: '',
+        loading: true
+    }
+
+    constructor() {
+        super();
+        const subs = this.state.subs;
+        this.state.sub = this.getRandomSub(subs);
+    }
+
+    getRandomSub(subs) {
+        return subs[Math.floor(Math.random() * subs.length)]
+    }
+
+    componentDidMount() {
+        this.setState({ subPosts: [] });
+        this.fetchSub();
+    }
+
+    componentWillUnmount()
+    {
+    }
+
+    fetchSub() {
+        this.setState({ loading: true });
+        axios.get(`https://www.reddit.com/r/${this.state.sub}.json`).then(res => {
+            this.setState({ subPosts: res.data.data.children.map(obj => obj.data), loading: false });
+        }).catch(err => {
+            console.error(err+" : fetching another sub");
+            this.getRandomSub(this.state.subs);
+            this.fetchSub();    
+        });
+    }
+
+    changeSubs = e => {
+        if (e.target.value == null) {
+            return;
+        }
+
+        const subs = e.target.value.split(',');
+        this.setState({ subs: subs, sub: this.getRandomSub(subs) });
+
+        this.fetchSub();
+    }
+
+    getList(posts) {
+        if(this.state.loading)
+        {
+            return <div> Loading ... </div>;
+        }
+
+        return (
+            <ul>
+                {posts.map(post => {
+                    return (
+                        <li key={post.id}>
+                            <a href={`${post.url}`}>{post.title}</a> - by <a target="_blank" rel="noopener noreferrer" href={`https://reddit.com/u/${post.author}`}>{post.author}</a>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    render() {
+        const subs = this.state.subs;
+        const posts = this.state.subPosts;
+        return (
+            <div>
+                <label>SubReddits: </label>
+                <input id="sublist" onBlur={this.changeSubs} defaultValue={subs.join(',')} />
+                <h1>/r/{this.state.sub}</h1>
+                {this.getList(posts)}
+            </div>
+        );
+    }
+}
+
+export default Reddit;
